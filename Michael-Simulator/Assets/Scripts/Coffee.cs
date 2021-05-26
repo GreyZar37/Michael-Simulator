749f3ce35c5adb195e070f5ae6bde39766c2ca50
lindejacob <43791907+lindejacob@users.noreply.github.com>
2021-05-26 08:15:20 +0200

    Coffee and enemy start

--- Michael-Simulator/Assets/Scripts/Coffee.cs
+++ Michael-Simulator/Assets/Scripts/Coffee.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Coffee : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int wetness;
+    public int maxRefreshmentLevel;
+    public int currentBulletsLeft;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            currentBulletsLeft = maxRefreshmentLevel;
+        }
     }
 }
